7360e3209c19cbf63f3187e9960c23d99ff03841
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-06-16 20:38:26 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -9,20 +9,25 @@ public class FibonacciSeries {
 		System.out.print("Enter the Number For Which You want Fibonacci Series : ");
 		
 		int number = sc.nextInt();
-		System.out.print("Fibonacci Series for "+number +" is : [ ");
+		
 		int first = 0;
 		int second = 1;
 		
 		if(number == 0)
 		{ 
+			System.out.println("Please enter positive integer Number");
+			System.exit(0);
+		}
+		System.out.print("Fibonacci Series for "+number +" is : [ ");
+		if(number == 1) {
 			System.out.println(first + " ]");
 		}
-		if(number == 1) {
+		else if(number == 2) {
 			System.out.println(first + " , "+ second + " ]");
 		}
 		else {
 			System.out.print(first + " , " + second );
-			while(number > 1) {
+			while(number > 2) {
 				int next = first + second;
 				System.out.print(" , " + next );
 				first= second;
