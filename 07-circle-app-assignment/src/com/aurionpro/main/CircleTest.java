7d4424864d0ca0c5f8c16fc867f40dbf633ab7b3
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-06-25 18:12:58 +0530

    Circle app Assignment Ritesh

---
+++ 07-circle-app-assignment/src/com/aurionpro/main/CircleTest.java
@@ -0,0 +1,31 @@
+package com.aurionpro.main;
+
+import com.aurionpro.model.BorderType;
+import com.aurionpro.model.ColorType;
+import com.aurionpro.model.Circle;
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		
+		Circle circles[] = {
+				new Circle(-2, ColorType.BLUE, BorderType.DASHED),
+				new Circle(200, ColorType.GREEN),
+				new Circle(20),
+				new Circle(),
+				new Circle(30, ColorType.RED, BorderType.DOTTED),
+				};
+		
+		for(Circle c : circles)
+			printObjectDetail(c);
+	}
+
+	public static void printObjectDetail(Circle circle) {
+		System.out.println("Circle Radius is : " + circle.getRadius());
+		System.out.println("Circle Color is : " + circle.getColor());
+		System.out.println("Circle Border is : " + circle.getBorder());
+		System.out.println("Area of Big Rectangle is : " + circle.calculateArea());
+		System.out.println("\n***************************************************\n");
+	}
+
+}
