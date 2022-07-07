e25fb25b08f58e833e5c0144f38b106b69475044
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-07-07 13:31:43 +0530

    Number Guessing Game

--- 10-number-guessing-assignment/src/com/assignment/model/NumberGuessing.java
+++ 10-number-guessing-assignment/src/com/assignment/model/NumberGuessing.java
@@ -6,19 +6,44 @@ import java.util.Scanner;
 public class NumberGuessing {
 	private int turn = 1;
 	static Scanner sc = new Scanner(System.in);
+	String name;
 
 	public void play(String name) {
+		this.name = name;
 		int number = getRandomNumber(1, 100);
 //		System.out.println(number);
-		System.out.println("Hello : " + name + " You have 6 chance to guess the Number");
+		System.out.println("\nHello : " + name + " You have 6 chance to guess the Number");
 		System.out.println("number is Between 1 - 100");
 
 		while (turn <= 6) {
 			guessing(number);
 		}
 
-		System.out.println("\nYour Turns are ends, thank you for Playing : " + name);
+		System.out.println("\nYour Turns are ends, thank you " + name + " for Playing. ");
+		wishToPlay();
+	}
 
+	private void wishToPlay() {
+		String choice;
+		do {
+			System.out.print("Do you Want Play again (y/n) : ");
+			String userChoice = sc.next();
+			choice = playAgain(userChoice);
+		} while (choice != "n");
+
+		System.out.println("\n************GAME-ENDS********************");
+
+	}
+
+	private String playAgain(String userChoice) {
+		if (userChoice.equalsIgnoreCase("y")) {
+			turn = 1;
+			play(name);
+		} else if (!userChoice.equalsIgnoreCase("n")) {
+			System.err.println("invalid input!!! Please Try again!!.");
+			return null;
+		}
+		return "n";
 	}
 
 	private void guessing(int number) {
@@ -34,7 +59,7 @@ public class NumberGuessing {
 			turn++;
 		} else {
 			System.out.println("\nCongrats, You Guess Correct Number in :" + turn + " Turns");
-			System.exit(0);
+			wishToPlay();
 		}
 	}
 
